748ea416a397f254fcf9064c31ea6ea919f37a6c
celis123 <38595620+celis123@users.noreply.github.com>
2018-06-23 18:13:54 -0400

    change

--- assignments/lesson9/weather-site/js/wu-weather.js
+++ assignments/lesson9/weather-site/js/wu-weather.js
@@ -1,19 +1,20 @@
-let weatherRequest = new XMLHttpRequest();
+var weatherRequest = new XMLHttpRequest();
 
-weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=d9b4dd53b851e70eb542b0473ad439b1', true);
+weatherRequest.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=4156210&APPID=d9b4dd53b851e70eb542b0473ad439b1&units=imperial', true);
+
 
 weatherRequest.send();
 
 weatherRequest.onload = function() {
     
-    let weatherData = JSON.parse(weatherRequest.responseText);
+    var weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
-    document.getElementById('WDescription').innerHTML = weatherData.weather.id;
-    document.getElementById('curTemp').innerHTML = weatherData.main.temp;
-    document.getElementById('high').innerHTML = weatherData.main.temp_max;
-    document.getElementById('low').innerHTML = weatherData.main.temp_min;
-    document.getElementById('weatherIco').src = weatherData.weather.icon;
-    document.getElementById('todayPrecipitacion').innerHTML = weatherData.rain.3h;
-    document.getElementById('speed').innerHTML = weatherData.wind.speed;
-    document.getElementById('WindDirection').innerHTML = weatherData.wind.deg;
+    document.getElementById('WDescription').innerHTML = weatherData.list["0"].weather["0"].description;
+    document.getElementById('curTemp').innerHTML = weatherData.list["0"].main.temp;
+    document.getElementById('high').innerHTML = weatherData.list["0"].main.temp_max;
+    document.getElementById('low').innerHTML = weatherData.list["0"].main.temp_min;
+    document.getElementById('speed').innerHTML = weatherData.list["0"].wind.speed;
+    var iconcode = weatherData.list["0"].weather["0"].icon;
+    var icon_path= "https://openweathermap.org/img/w/" + iconcode+ ".png";
+    document.getElementById('weatherIco').src = icon_path;
 }
