b4c2d1de3b435cc86973ddd85199d2485d0e038a
celis123 <38595620+celis123@users.noreply.github.com>
2018-07-23 20:41:34 -0400

    changes

--- term-proyect/js/slider.js
+++ term-proyect/js/slider.js
@@ -1,32 +1,29 @@
 $(document).ready(function(){
-	var imgItems = $('.slider li').length; // Numero de Slides
+	var imgItems = $('.slider li').length;
 	var imgPos = 1;
 
-	// Agregando paginacion --
 	for(i = 1; i <= imgItems; i++){
 		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
 	} 
-	//------------------------
 
-	$('.slider li').hide(); // Ocultanos todos los slides
-	$('.slider li:first').show(); // Mostramos el primer slide
-	$('.pagination li:first').css({'color': '#CD6E2E'}); // Damos estilos al primer item de la paginacion
+	$('.slider li').hide();
+	$('.slider li:first').show();
+	$('.pagination li:first').css({'color': '#CD6E2E'});
 
-	// Ejecutamos todas las funciones
 	$('.pagination li').click(pagination);
 	$('.right span').click(nextSlider);
 	$('.left span').click(prevSlider);
 
-
-	// FUNCIONES =========================================================
+	setInterval(function(){
+		nextSlider();
+	}, 10000);
 
 	function pagination(){
-		var paginationPos = $(this).index() + 1; // Posicion de la paginacion seleccionada
+		var paginationPos = $(this).index() + 1;
 
-		$('.slider li').hide(); // Ocultamos todos los slides
-		$('.slider li:nth-child('+ paginationPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		$('.slider li').hide();
+		$('.slider li:nth-child('+ paginationPos +')').fadeIn();
 
-		// Dandole estilos a la paginacion seleccionada
 		$('.pagination li').css({'color': '#858585'});
 		$(this).css({'color': '#CD6E2E'});
 
@@ -41,8 +38,8 @@ $(document).ready(function(){
 		$('.pagination li').css({'color': '#858585'});
 		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
 
-		$('.slider li').hide(); // Ocultamos todos los slides
-		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		$('.slider li').hide();
+		$('.slider li:nth-child('+ imgPos +')').fadeIn();
 
 	}
 
@@ -53,8 +50,8 @@ $(document).ready(function(){
 		$('.pagination li').css({'color': '#858585'});
 		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
 
-		$('.slider li').hide(); // Ocultamos todos los slides
-		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		$('.slider li').hide();
+		$('.slider li:nth-child('+ imgPos +')').fadeIn();
 	}
 
 });
